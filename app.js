da2c65658847228df5e01e34e199b65472ef22c6
Eom Hwiyong <eomhy@osci.kr>
2018-07-18 17:48:21 +0900

    컨디션 로직 2/7 을 작성한다

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const { condition, hourly } = require('./src/wunderground');
+const { condition, hourly, weather } = require('./src/wunderground');
 
 // console.log(condition('seoul'));
-hourly('seoul').then(data => {
-  console.log('data: ', data);
-});
+// hourly('seoul').then(data => {
+//   console.log('data: ', data);
+// });
+weather('seoul').then(result => console.log(result));
--- src/wunderground.js
+++ src/wunderground.js
@@ -26,9 +26,16 @@ const condition = (loc = '') => {
       url: url
     })
       .then(res => {
-        resolve(res.data);
+        if (!('error' in res)) {
+          console.log('resolve in condition');
+          resolve(res.data);
+        } else {
+          console.log('reject in condition');
+          reject(res);
+        }
       })
       .catch(err => {
+        console.log('reject in condition');
         reject(res);
       });
   });
@@ -46,13 +53,95 @@ const hourly = (loc = '') => {
       url: url
     })
       .then(res => {
-        resolve(res.data);
+        if (!('error' in res)) {
+          console.log('resolve in hourly');
+          resolve(res.data);
+        } else {
+          console.log('reject in hourly');
+          reject(res);
+        }
       })
       .catch(err => {
+        console.log('reject in hourly');
         reject(res);
       });
   });
 };
 
+/**
+ *
+ * @param {string} loc
+ */
+const weather = async (loc = '') => {
+  // get condition data
+  let maybe_new_location = '';
+  let cd = 'error';
+  try {
+    res = await condition(loc);
+    if ('error' in res) {
+      // 에러 결과
+      1 / 0;
+    } else if ('results' in res['response']) {
+      // 리스트 결과
+      maybe_new_location = 'zmw:' + res['response']['results'][0]['zmw'];
+      condition(maybe_new_location)
+        .then(res_ => {
+          if ('error' in res_) {
+            1 / 0;
+          } else {
+            cd = res_.data;
+          }
+        })
+        .catch(err => {
+          1 / 0;
+        });
+    } else {
+      // 정상 결과
+      cd = res.data;
+    }
+  } catch (exc) {
+    cd = 'error';
+  }
+
+  // get hourly data
+  maybe_new_location = maybe_new_location === '' ? loc : maybe_new_location;
+  let hd = 'error';
+  try {
+    res = await hourly(loc);
+
+    if ('error' in res) {
+      // 에러 결과
+      1 / 0;
+    } else {
+      // 정상 결과
+      hd = res.data;
+    }
+  } catch (exc) {
+    hd = 'error';
+  }
+
+  // assemble
+  let ret = '';
+
+  if (cd === 'error') {
+    ret += `${null}, ${null}, ${null}\n${null} ${null}°C ${null} rh ${null}\n`;
+  } else {
+    const cdco = cd['current_observation'];
+    ret += `${cdco['observation_time_rfc822']
+      .split(' ')[0]
+      .substring(-1)}, ${null}, ${
+      cdco['display_location']['full']
+    }\n${null} ${null}°C ${null} rh ${null}\n`;
+  }
+
+  if (hd === 'error') {
+    ret += '{} {}°C{}\n';
+  } else {
+    ret += 'LOGIC\n';
+  }
+
+  return ret;
+};
+
 // export { condition };
-module.exports = { condition, hourly };
+module.exports = { condition, hourly, weather };
