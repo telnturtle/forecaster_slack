02f4e00d3771e5498d0e72b0185e189c514f9ace
eomhy <telnturtle@gmail.com>
2018-07-19 22:02:02 +0900

    익스포트 추가

--- src/slack.js
+++ src/slack.js
@@ -10,9 +10,4 @@ const curl = async payload => {
   });
 };
 
-// for test
-curl(
-  '안녕하세요 js 입니다. 윈도우 로컬에 있습니다. url 입력 테스트. ' + Date()
-).then(res => {
-  console.log(res.data);
-});
+module.exports = { curl };
