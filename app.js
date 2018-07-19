c72adf9f074d09d8ea94c008e6d5a54aad752b3a
eomhy <telnturtle@gmail.com>
2018-07-19 22:09:51 +0900

    테스트 스케줄링 추가

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const { weather_emoji } = require('./src/wunderground');
+const { curl } = require('./src/slack');
 const cron = require('node-cron');
 
 const job = async () => {
   const p = await weather_emoji('seoul');
-  console.log(p);
+  curl(
+    '22:15 발송 테스트입니다.\n' + '[payload]'
+  );
+  console.log(
+    '22:15 발송 테스트입니다.\n' + '[payload]'
+  );
 };
 
-cron.schedule('0 21 * * 0-4', job); // cron.schedule('0 6 * * 1-5', job);
+// cron.schedule('0 21 * * 0-4', job); // cron.schedule('0 6 * * 1-5', job);
+
+// for test
+cron.schedule('0,30 13,14 * * 1-5', job);
