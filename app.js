0a014a004f89e918252a6670f9df07a391e1c6e2
eomhy <telnturtle@gmail.com>
2018-07-23 22:27:34 +0900

    Change printing

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const schedule = require('node-schedule');
 const job = async () => {
   const payload = await weather_emoji('seoul');
   curl(payload).then(res => {
-    console.log(payload);
+    console.log(payload, '\n');
   });
 };
+
+console.log('Forecaster\n', Date(), '\n');
 
 const ssj = schedule.scheduleJob('0 21 * * 0-4', job); // GMT
 // const ssj = schedule.scheduleJob('0 6 * * 1-5', job); // KST
