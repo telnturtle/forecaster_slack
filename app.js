47435742e7aabe71126362fa94f02daed88ee2ff
eomhy <telnturtle@gmail.com>
2018-07-23 22:13:05 +0900

    Replace scheduler
    node-cron -> node-schedule.

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const { weather_emoji } = require('./src/wunderground');
 const { curl } = require('./src/slack');
-const cron = require('node-cron');
+const schedule = require('node-schedule');
 
 const job = async () => {
   const p = await weather_emoji('seoul');
-  curl(
-    '22:15 발송 테스트입니다.\n' + '[payload]'
-  );
-  console.log(
-    '22:15 발송 테스트입니다.\n' + '[payload]'
-  );
+  curl(p).then(res => {
+    console.log(p);
+  });
 };
 
-// cron.schedule('0 21 * * 0-4', job); // cron.schedule('0 6 * * 1-5', job);
-
-// for test
-cron.schedule('0,30 13,14 * * 1-5', job);
+cron.schedule('0 21 * * 0-4', job); // GMT
+// cron.schedule('0 6 * * 1-5', job); // KST
