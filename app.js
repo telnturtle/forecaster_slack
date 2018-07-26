3f1fa50ecb28b9e33aa12135e70f55753d8ed6f0
Eom Hwiyong <eomhy@osci.kr>
2018-07-26 10:06:38 +0900

    Add try-catch

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const { curl } = require('./src/slack');
 const schedule = require('node-schedule');
 
 const job = async () => {
-  const payload = await weather_emoji('seoul');
-  curl(payload).then(res => {
-    console.log(payload, '\n');
-  });
+  try {
+    const payload = await weather_emoji('seoul');
+    curl(payload).then(res => {
+      console.log(payload, '\n');
+    });
+  } catch (e) {
+    console.log('in app: ', e);
+  }
 };
 
 console.log('Forecaster\n', Date(), '\n');
 
-const ssj = schedule.scheduleJob('0 21 * * 0-4', job); // GMT
-// const ssj = schedule.scheduleJob('0 6 * * 1-5', job); // KST
+try {
+  const ssj = schedule.scheduleJob('0 21 * * 0-4', job); // GMT
+  // const ssj = schedule.scheduleJob('0 6 * * 1-5', job); // KST
+  // const ssj2 = schedule.scheduleJob('*/1 * * * *', () => {
+  //   console.log(Date());
+  // }); // KST
+} catch (e) {
+  console.log('in loop: ', e);
+}
